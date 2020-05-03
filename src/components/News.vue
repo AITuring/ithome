5e7ad346821cbe5f9feb2b8f6683044e4bb99920
AITuring <shspku365@gmail.com>
2020-05-04 03:08:28 +0800

    init

---
+++ src/components/News.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="news">
+        <h1>今日新闻</h1>
+        <div class="single-news" v-for="singleNews in allNews" :key="singleNews">
+            <h2>{{singleNews.title}}</h2>
+            <article>{{singleNews.description}}</article>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+  name: 'News',
+  data () {
+    return {
+      allNews: []
+    }
+  },
+  created () {
+    this.$http.get('https://api.ithome.com/json/newslist/news?r=0')
+      .then(function (data) {
+        console.log(data)
+        this.allNews = data
+      })
+  }
+}
+
+</script>
+
+<style scoped>
+
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,17 @@
+import Vue from 'vue'
+import Router from 'vue-router'
+// import HelloWorld from '@/components/HelloWorld'
+import News from '@/components/News'
+
+Vue.use(Router)
+
+export default new Router({
+  mode: 'history',
+  routes: [
+    {
+      path: '/',
+      name: 'News',
+      component: News
+    }
+  ]
+})
